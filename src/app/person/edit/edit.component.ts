910d3205c68ba0f01d2f473590536840c83da236
JotaPrimo <gestald118@gmail.com>
2022-11-06 17:59:38 -0400

    /edit.component.ts

--- src/app/person/edit/edit.component.ts
+++ src/app/person/edit/edit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Person } from '../person';
+import { PersonService } from '../person.service';
 
 @Component({
   selector: 'app-edit',
@@ -7,9 +11,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditComponent implements OnInit {
 
-  constructor() { }
+  idPerson: number | any;
+  form: FormGroup | any;
+  person: Person | any;
+
+  constructor(
+    public personService: PersonService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.idPerson = this.route.snapshot.params['idPerson'];
+    this.personService.find(this.idPerson).subscribe(
+      (data: Person) => {
+        this.person = data
+      }
+    );
+
+    this.form = new FormGroup({
+      name:  new FormControl('', [ Validators.required, Validators.pattern('^[a-zA-ZÁáÀàÉéÈèÍíÌìÓóÒòÚúÙùÑñüÜ \-\']+') ]),
+      email: new FormControl('', [ Validators.required, Validators.email]),
+      phone: new FormControl('', [ Validators.required, Validators.pattern("^[0-9]*$") ])
+    });
+
   }
+
+  get f() {
+    return this.form.controls;
+  }
+
+  submit() {
+    console.log(this.form.value);
+    this.personService.update(this.idPerson, this.form.value).subscribe(
+      res => {
+        console.log('Person updated successfully!');
+        this.router.navigateByUrl('person/index');
+      }
+    )
+  }
+
 
 }
